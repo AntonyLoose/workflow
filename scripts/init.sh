e87d55be7e4c622691d9d2726ce9c31618fad584
Antony Loose <antony35007@gmail.com>
2025-06-10 06:48:33 +0000

    refactor: use ssh repo urls

--- scripts/init.sh
+++ scripts/init.sh
@@ -73,7 +73,7 @@ echo "---------- i3 ----------"
 echo ""
 pacman -S --noconfirm i3-wm
 rm -rf /home/"$name"/.config/i3
-git clone github.com/AntonyLoose/i3-config /home/"$name"/.config/i3
+git clone git@github:AntonyLoose/i3-config /home/"$name"/.config/i3
 chown -R "$name:$name" /home/"$name"/.config/i3
 
 
@@ -81,7 +81,7 @@ echo "---------- picom ----------"
 echo ""
 pacman -S --noconfirm picom
 rm -rf /home/"$name"/.config/picom
-git clone https://github.com/AntonyLoose/picom-config /home/"$name"/.config/picom
+git clone git@github.com:AntonyLoose/picom-config /home/"$name"/.config/picom
 chown -R "$name:$name" /home/"$name"/.config/picom
 
 
@@ -89,7 +89,7 @@ echo "---------- Kitty ----------"
 echo ""
 pacman -S --noconfirm kitty
 rm -rf /home/"$name"/.config/kitty
-git clone https://github.com/AntonyLoose/kitty-config /home/"$name"/.config/kitty
+git clone git@github.com:AntonyLoose/kitty-config /home/"$name"/.config/kitty
 chown -R "$name:$name" /home/"$name"/.config/kitty
 
 
