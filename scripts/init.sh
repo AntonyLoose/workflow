75a9afa217f14b119622557630c65868d5a9f71f
Antony Loose <antony35007@gmail.com>
2025-06-11 17:57:34 +1000

    refactor: init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -20,20 +20,21 @@ fi
 
 echo ""
 echo "---------- Creating User ----------"
-read -p "Username: " name
+read -rp "Username: " name
 
 useradd -m -G wheel -s /bin/bash "$name"
 echo "Set password for $name:"
 passwd "$name"
 
 echo ""
+echo "---------- Installing sudo ----------"
+pacman -Sy --noconfirm sudo
+
+echo ""
 echo "---------- Copying SSH keys ----------"
-mkdir -p /home/"$name"/.ssh
-cp /root/.ssh/github /home/"$name"/.ssh/
-cp /root/.ssh/github.pub /home/"$name"/.ssh/
-chown -R "$name":"$name" /home/"$name"/.ssh
-chmod 700 /home/"$name"/.ssh
-chmod 600 /home/"$name"/.ssh/github /home/"$name"/.ssh/github.pub
+install -d -m 700 -o "$name" -g "$name" "/home/$name/.ssh"
+install -m 600 -o "$name" -g "$name" /root/.ssh/github "/home/$name/.ssh/github"
+install -m 600 -o "$name" -g "$name" /root/.ssh/github.pub "/home/$name/.ssh/github.pub"
 
 echo ""
 echo "---------- Security ----------"
@@ -56,50 +57,61 @@ echo "----- Disabling root access via SSH -----"
 echo "PermitRootLogin no" > /etc/ssh/sshd_config.d/20-deny-root.conf
 
 echo ""
-echo "---------- Installing sudo ----------"
-pacman -Sy --noconfirm sudo
-
-echo ""
 echo "---------- Installing base packages ----------"
 pacman -Sy --noconfirm \
   less nodejs npm neovim lua-language-server texlab xclip \
-  i3-wm i3status picom kitty fastfetch firefox
+  i3-wm i3status picom kitty fastfetch firefox \
+  networkmanager keychain base-devel dmenu feh \
+  xorg xorg-server xorg-xinit pipewire pipewire-pulse \
+  wireplumber pipewire-alsa
 
 echo ""
 echo "---------- Setting up user environment ----------"
 
 run_as_user() {
-  sudo -u "$name" bash -c "$1"
+  sudo -u "$name" -- bash -c "$1"
 }
 
-# Clone user config repos as the new user
-run_as_user 'eval "($ssh-agent -s)"'
-run_as_user "ssh-add ~/.ssh/github"
-run_as_user "git clone git@github.com:AntonyLoose/nvim-config.git /home/$name/.config/nvim"
-run_as_user "git clone git@github.com:AntonyLoose/i3-config.git /home/$name/.config/i3"
-run_as_user "git clone git@github.com:AntonyLoose/i3status-config.git /home/$name/.config/i3status"
-run_as_user "git clone git@github.com:AntonyLoose/picom-config.git /home/$name/.config/picom"
-run_as_user "git clone git@github.com:AntonyLoose/kitty-config.git /home/$name/.config/kitty"
-run_as_user "git clone git@github.com:AntonyLoose/dotfiles.git /home/$name/dotfiles"
+# Persist SSH keychain setup
+run_as_user "echo 'eval \$(keychain --eval ~/.ssh/github)' >> ~/.bash_profile"
+
+# Clone config repos as the new user
+run_as_user "git clone git@github.com:AntonyLoose/nvim-config.git ~/.config/nvim"
+run_as_user "git clone git@github.com:AntonyLoose/i3-config.git ~/.config/i3"
+run_as_user "git clone git@github.com:AntonyLoose/i3status-config.git ~/.config/i3status"
+run_as_user "git clone git@github.com:AntonyLoose/picom-config.git ~/.config/picom"
+run_as_user "git clone git@github.com:AntonyLoose/kitty-config.git ~/.config/kitty"
+run_as_user "git clone git@github.com:AntonyLoose/dotfiles.git ~/dotfiles"
 
 # Fix ownership
-chown -R "$name":"$name" /home/"$name"/.config /home/"$name"/dotfiles
+chown -R "$name:$name" "/home/$name/.config" "/home/$name/dotfiles"
 
 # Setup dotfiles symlinks
-run_as_user "rm -f /home/$name/.bashrc /home/$name/.bash_profile"
-run_as_user "ln -s /home/$name/dotfiles/.bashrc /home/$name/.bashrc"
-run_as_user "ln -s /home/$name/dotfiles/.bash_profile /home/$name/.bash_profile"
-run_as_user "chown -h $name:$name /home/$name/.bashrc /home/$name/.bash_profile"
+run_as_user "rm -f ~/.bashrc ~/.bash_profile"
+run_as_user "ln -s ~/dotfiles/.bashrc ~/.bashrc"
+run_as_user "ln -s ~/dotfiles/.bash_profile ~/.bash_profile"
 
-# Setup GTK dark theme preference
-run_as_user "mkdir -p /home/$name/.config/gtk-3.0"
-run_as_user "echo '[Settings]' > /home/$name/.config/gtk-3.0/settings.ini"
-run_as_user "echo 'gtk-application-prefer-dark-theme=true' >> /home/$name/.config/gtk-3.0/settings.ini"
+# GTK dark theme
+run_as_user "mkdir -p ~/.config/gtk-3.0 && echo -e '[Settings]\ngtk-application-prefer-dark-theme=true' > ~/.config/gtk-3.0/settings.ini"
+
+# Set i3 to run on login
+run_as_user "echo 'exec i3' > ~/.xinitrc"
+
+# Enable pipewire user services
+run_as_user "systemctl --user enable pipewire pipewire-pulse"
+
+# Setup timezone
+timedatectl set-ntp true
+systemctl restart systemd-timesyncd
+timedatectl set-timezone Australia/Melbourne
 
 echo ""
-echo "----- Installing additional language servers and npm packages -----"
-pacman -Sy --noconfirm lua-language-server texlab
+echo "----- Installing npm language servers -----"
 run_as_user "npm install -g typescript typescript-language-server vscode-langservers-extracted css-variables-language-server bash-language-server"
 
 echo ""
-echo "----- Done! User $name is set up with your configs and packages -----"
+echo "----- Done! User $name is set up with your configs and packages. -----"
+
+echo ""
+echo "----- Rebooting -----"
+reboot
