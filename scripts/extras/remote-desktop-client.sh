7b3ca3a1201af9df84bff0e7defb8c91841c664e
Antony Loose <antony35007@gmail.com>
2025-08-12 21:24:16 +1000

    fix: ssh key gen for remote desktop client setup

--- scripts/extras/remote-desktop-client.sh
+++ scripts/extras/remote-desktop-client.sh
@@ -5,7 +5,7 @@ sudo pacman -Sy openssh xorg-xauth
 read -rp "Enter your email: " email
 read -rp "File path to save ssh key: " name
 
-ssh-keygen -t rsa -b 4096 -C -f "$name"
+ssh-keygen -t rsa -b 4096 -C "$email" -f "$name"
 
 echo "Copy the following public key and add it to the remote."
 
