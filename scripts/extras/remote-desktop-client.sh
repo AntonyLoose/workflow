201d8930a0570836e710f77c5ce0fbac2b51fd07
Antony Loose <antony35007@gmail.com>
2025-08-12 21:31:59 +1000

    fix: file path for key creation

--- scripts/extras/remote-desktop-client.sh
+++ scripts/extras/remote-desktop-client.sh
@@ -5,9 +5,9 @@ sudo pacman -Sy openssh xorg-xauth
 read -rp "Enter your email: " email
 read -rp "File path to save ssh key: " name
 
-ssh-keygen -t rsa -b 4096 -C "$email" -f "$name"
+ssh-keygen -t rsa -b 4096 -C "$email" -f "$HOME/.ssh/$name"
 
 echo "Copy the following public key and add it to the remote."
 
-cat "$name.pub"
+cat "$HOME/.ssh/$name.pub"
 
