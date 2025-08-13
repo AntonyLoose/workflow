a4e451afb7b6b622c9cb71f2354a521d43400d38
Antony Loose <antony35007@gmail.com>
2025-08-13 17:57:45 +1000

    fix: x0vncserver command

--- scripts/extras/server-vnc-gui.bash
+++ scripts/extras/server-vnc-gui.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# I use a weird geometry as my laptop resolution is weird.
-sudo x0vncserver -display :0 -localhost -alwaysshared -geometry 1366x768 -rfbauth ~/.config/tigervnc/passwd
+sudo x0vncserver -rfbauth ~/.config/tigervnc/passwd
 
