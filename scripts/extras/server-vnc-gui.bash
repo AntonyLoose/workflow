81f4d0352ca3a22bd9a80aa1d2e5c148522ae6f4
Antony Loose <antony35007@gmail.com>
2025-08-13 16:26:53 +1000

    fix: vcstiger server setup

--- scripts/extras/server-vnc-gui.bash
+++ scripts/extras/server-vnc-gui.bash
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Make sure to set `localhost` in $XDG_CONFIG_HOME/tigervnc/config, otherwise, the
-# connection will be insecure.
-
-sudo systemctl start vncserver@:1.service
-sudo x0vncserver -rfbauth ~/.config/tigervnc/passwd
+sudo x0vncserver -display :0 -localhost -alwaysshared -geometry 1920x1080 -rfbauth ~/.config/tigervnc/passwd
 
