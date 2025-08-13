b092c3bff5cca30454c26418e230915151428156
Antony Loose <antony35007@gmail.com>
2025-08-13 14:47:57 +1000

    refactor: revert remote desktop changes

--- scripts/extras/remote-desktop-client.sh
+++ scripts/extras/remote-desktop-client.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 sudo pacman -Sy openssh xorg-xauth
-yay -Sy rdesktop
-
-# Connect to a remote desktop (running an RDP server) with the following command:
-# rdesktop -g 1440x900 -P -z -x l -r sound:off -u <username> <ip>:3389
-# more info here: https://wiki.archlinux.org/title/Rdesktop
 
 read -rp "Enter your email: " email
 read -rp "File path to save ssh key: " name
--- scripts/extras/xrpd.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-yay -Sy xrdp xorgxrdp
-
