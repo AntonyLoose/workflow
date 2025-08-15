8814e2c58e74f8649a0a1cc39aac61957f3ada91
Antony Loose <antony35007@gmail.com>
2025-08-15 19:14:13 +1000

    feat: start ssh on vnc server start

--- scripts/extras/client-vnc-gui.bash
+++ scripts/extras/client-vnc-gui.bash
@@ -5,4 +5,4 @@ read -rp "Location of key: " key
 read -rp "Port of user session on remote: " port # I have configured to 5901
 
 # A one liner for port forwarding during the connection and closing it right after
-ssh -fL 9901:localhost:$port $ip -i $key sleep 10; vncviewer localhost:9901
+ssh -fL 9901:localhost:$port $ip -i $key sleep 10; vncviewer localhost:9901 &
--- scripts/extras/server-vnc-gui.bash
+++ scripts/extras/server-vnc-gui.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo systemctl start sshd
 sudo x0vncserver -display :0 -localhost -alwaysshared -geometry 1920x1080 -rfbauth ~/.config/tigervnc/passwd
 
